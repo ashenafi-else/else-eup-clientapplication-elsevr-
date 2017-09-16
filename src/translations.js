a60dad5dee4537def4a5ac4e8c7689be90deab5a
Andrey Vinogradov <andrey.vinogradov@else-corp.it>
2017-09-16 12:00:58 +0200

    translations for materials

--- src/translations.js
+++ src/translations.js
@@ -44,7 +44,265 @@ window.translations = {
     'MATERIAL_LIBRARY_VIEW': {
         'en': 'MATERIALS'
     },
+    'MAT_VELUKID_10667': {
+        'en': 'Pink'
+    },
+    'MAT_VELUKID_10668': {
+        'en': 'Antique pink'
+    },
+    'MAT_VELUKID_10675': {
+        'en': 'Purple'
+    },
+    'MAT_VELUKID_10683': {
+        'en': 'Blue electric'
+    },
+    'MAT_SUEDE_BEIGE': {
+        'en': 'Beige'
+    },
+    'MAT_SUEDE_WHITE': {
+        'en': ''
+    },
+    'MAT_SUEDE_BLUE': {
+        'en': 'Blue'
+    },
+    'MAT_SUEDE_RED': {
+        'en': 'Red'
+    },
+    'MAT_TURQUOISE_BLUESHADE': {
+        'en': 'Turquoise blue shade'
+    },
+    'MAT_TURQUOISE_GREENSHADE': {
+        'en': 'Turquoise green shade'
+    },
+    'MAT_SUEDE_BLACK': {
+        'en': 'Black'
+    },
+    'MAT_NAPPA_WHITE': {
+        'en': 'White'
+    },
+    'MAT_NAPPA_PINK': {
+        'en': 'Pink'
+    },
+    'MAT_NAPPA_BLUE-NAVY': {
+        'en': 'Blue navy'
+    },
+    'MAT_NAPPA_LIGHTBEIGE': {
+        'en': 'Light beige'
+    },
+    'MAT_NAPPA_GOLD': {
+        'en': 'Gold'
+    },
+    'MAT_NAPPA_RED': {
+        'en': 'Red'
+    },
+    'MAT_NAPPA_BLACK': {
+        'en': 'Black'
+    },
+    'MAT_PATENT_35': {
+        'en': 'Black'
+    },
+    'MAT_PATENT_37': {
+        'en': 'Turquoise'
+    },
+    'MAT_PATENT_1': {
+        'en': 'Pink'
+    },
+    'MAT_PATENT_3': {
+        'en': 'Blue navy'
+    },
+    'MAT_PATENT_2': {
+        'en': 'Sky blue'
+    },
+    'MAT_PATENT_10667': {
+        'en': 'Antique pink'
+    },
+    'MAT_PATENT_10652': {
+        'en': 'Nude 1'
+    },
+    'MAT_PATENT_10653': {
+        'en': 'Nude 2'
+    },
+    'MAT_PATENT_10671': {
+        'en': 'Nude 3'
+    },
+    'MAT_PATENT_10675': {
+        'en': 'Nude 4'
+    },
+    'MAT_PATENT_10679': {
+        'en': 'Red'
+    },
+    'MAT_MIRRORPATENT_SILVER': {
+        'en': 'Silver'
+    },
+    'MAT_MIRRORPATENT_GOLD': {
+        'en': 'Gold'
+    },
+    'MAT_RUBBER_BROWN': {
+        'en': ''
+    },
+    'MAT_METAL_GOLD': {
+        'en': 'Gold'
+    },
+    'MAT_METAL_SILVER-BUCKLE': {
+        'en': 'Silver'
+    },
+    'MAT_METAL_SILVER': {
+        'en': 'Silver'
+    },
+    'MAT_CRYSTAL_ SILVERSHADE-OUTSIDE': {
+        'en': ''
+    },
+    'MAT_CRYSTAL_GOLDENSHADOW-OUTSDIE': {
+        'en': ''
+    },
+    'MAT_CRYSTAL_ SILVERSHADE-INSIDE': {
+        'en': ''
+    },
+    'MAT_CRYSTAL_GOLDENSHADOW-INSIDE': {
+        'en': ''
+    },
+    'MAT_CRYSTAL_ROSEGOLD': {
+        'en': 'Gold'
+    },
+    'MAT_LEATHER_BEIGE': {
+        'en': 'Beige'
+    },
+    'MAT_TRANSPARENT_LOGO-GOLD': {
+        'en': 'Gold'
+    },
+    'MAT_LEATHER_DARKBLUE': {
+        'en': ' Blue'
+    },
+    'MAT_LEATHER_BROWN-OUTSOLE': {
+        'en': ''
+    },
+    'MAT_CRYSTAL_METALLICBLUE': {
+        'en': ''
+    },
+    'MAT_METAL_TR-SILVER': {
+        'en': 'Silver'
+    },
+    'MAT_METAL_TR-GOLD': {
+        'en': 'Gold'
+    },
+    'MAT_CRYSTAL_CRYSTAL-INSIDE': {
+        'en': ''
+    },
+    'MAT_CRYSTAL_HYACINTH-INSIDE': {
+        'en': ''
+    },
+    'MAT_CRYSTAL_BLUEZIRCON-INSIDE': {
+        'en': ''
+    },
+    'MAT_CRYSTAL_ ANTIQUEPINK-INSIDE': {
+        'en': ''
+    },
+    'MAT_CRYSTAL_ LIGHTTURQUOISE-INSIDE': {
+        'en': ''
+    },
+    'MAT_CRYSTAL_ LIGHTROSE-INSIDE': {
+        'en': ''
+    },
+    'MAT_CRYSTAL_ROSE-INSIDE': {
+        'en': ''
+    },
+    'MAT_CRYSTAL_CRYSTAL-OUTSIDE': {
+        'en': ''
+    },
+    'MAT_CRYSTAL_ ANTIQUEPINK-OUTSIDE': {
+        'en': ''
+    },
+    'MAT_CRYSTAL_TRANSPARENT-OUTSIDE': {
+        'en': ''
+    },
+    'MAT_SUEDE_BLUE-MAN': {
+        'en': 'blue'
+    },
+    'MAT_SUEDE_BLACK-MAN': {
+        'en': 'black'
+    },
+    'MAT_SUEDE_INDIGO': {
+        'en': 'indigo'
+    },
+    'MAT_SUEDE_GREEN': {
+        'en': 'green'
+    },
+    'MAT_SUEDE_BEIGE-MAN': {
+        'en': 'beige'
+    },
+    'MAT_SUEDE_LIGHT-GREY': {
+        'en': 'light grey'
+    },
     'MAT_LEATHER_BLACK-MAN': {
-        'en': 'Black Leather'
+        'en': 'black'
+    },
+    'MAT_LEATHER_WHITE-MAN': {
+        'en': 'white'
+    },
+    'MAT_LEATHER_BORDEAUX-MAN': {
+        'en': 'bordeaux'
+    },
+    'MAT_LEATHER_BLUE-NAVY-MAN': {
+        'en': 'blue navy'
+    },
+    'MAT_LEATHER_DARK-BLUE': {
+        'en': 'sky blue'
+    },
+    'MAT_LEATHER_DARK-BROWN': {
+        'en': 'dark brown'
+    },
+    'MAT_LEATHER_LIGHT-BROWN': {
+        'en': 'light brown'
+    },
+    'MAT_PATENT-LEATHER_BLACK': {
+        'en': 'black'
+    },
+    'MAT_PATENT-LEATHER_WHITE': {
+        'en': 'white'
+    },
+    'MAT_GRAIN-LEATHER_DARK-BROWN': {
+        'en': 'dark brown'
+    },
+    'MAT_GRAIN-LEATHER_LIGHT-BROWN': {
+        'en': 'light brown'
+    },
+    'MAT_GRAIN-LEATHER_WHITE': {
+        'en': 'white'
+    },
+    'MAT_GRAIN-LEATHER_BLACK': {
+        'en': 'black'
+    },
+    'MAT_TRANSPARENT_TRANSPARENT': {
+        'en': ''
+    },
+    'MAT_LACE-FABRIC_BLACK': {
+        'en': 'black'
+    },
+    'MAT_LACE-FABRIC_DARK-BLUE': {
+        'en': 'dark blue'
+    },
+    'MAT_LACE-FABRIC_DARK-BROWN': {
+        'en': 'dark brown'
+    },
+    'MAT_LACE-FABRIC_LIGHT-BROWN': {
+        'en': 'light brown'
+    },
+    'MAT_SOLE-LEATHER_DARK-BLUE': {
+        'en': 'blue'
+    },
+    'MAT_RUBBER_BLACK': {
+        'en': 'black'
+    },
+    'MAT_SHINE_BLACK': {
+        'en': 'black'
+    },
+    'MAT_SUEDE_DARK-BEIGE': {
+        'en': 'dark beige'
+    },
+    'MAT_SUEDE_LIGHT-BLUE': {
+        'en': 'light blue'
+    },
+    'MAT_LEATHER_DARK-BEIGE': {
+        'en': 'dark beige'
     }
 };

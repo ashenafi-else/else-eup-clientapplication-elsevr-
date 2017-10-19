e4d068978f83f3dc7d7c0927f284b901ed2951f5
Mertcan <m.yucel@else-corp.it>
2017-10-19 10:59:47 +0200

    default config try

--- src/config.js
+++ src/config.js
@@ -16,7 +16,46 @@ window.config = {
     },
     'default_product_image_prefix': 'https://crm.else.shoes/pub/media/catalog/product',
     'default_collection_image_prefix': 'https://crm.else.shoes/pub/media/catalog/category/',
-    'default_configuration': {},
+    'default_configuration': {
+
+        'elsevr_men_1_1': [
+            {
+                'component': '1',
+                'structure': '1',
+                'element': '0',
+                'active_material': 'nappa',
+                'active_color': 'mat_nappa_red'
+            }
+        ],
+        'elsevr_women_1_1_1': [
+            {
+                'component': '1',
+                'structure': '1',
+                'element': '0',
+                'active_color': 'mat_nappa_red'
+            }, {
+                'component': '2',
+                'structure': '1',
+                'element': '1',
+                'active_color': 'mat_plastic_red'
+            }, {
+                'component': '3',
+                'structure': '1',
+                'element': '2',
+                'active_color': 'mat_plastic_red'
+            }
+        ],
+        'elsevr_women_1_1_2': [
+            {
+                'component': '1',
+                'structure': '1',
+                'element': '0',
+                'active_material': 'nappa',
+                'active_color': 'mat_nappa_red'
+            }
+        ],
+
+    },
     'product_tour_configuration': {
         'elsevr_women_1_1': {
         	'name': 'micam v2 model 1.2 (Product tour 122877)',

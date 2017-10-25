b465e302056e313cf2efc7b369c5673c686f45c6
Andrey Vinogradov <andrey.vinogradov@else-corp.it>
2017-10-25 19:38:00 +0200

    wrong structure was turned off by display none

--- src/config.js
+++ src/config.js
@@ -37,40 +37,6 @@ window.config = {
     'default_collection_image_prefix': 'https://crm.else.shoes/pub/media/catalog/category/',
     'default_configuration': {
 
-        'elsevr_women_1_1': [
-            {
-                'component': '1',
-                'structure': '1',
-                'element': '0',
-                'active_material': 'nappa',
-                'active_color': 'mat_nappa_red'
-            },
-            {
-                'component': '2',
-                'structure': '1',
-                'element': '0',
-                'active_material': 'nappa',
-                'active_color': 'mat_nappa_gold'
-            },
-            {
-                'component': '3',
-                'structure': '1',
-                'element': '0',
-                'active_material': 'nappa',
-                'active_color': 'mat_nappa_gold'
-            },
-            {
-                'component': '4',
-                'structure': '0',
-            },
-            {
-                'component': '10',
-                'structure': '3',
-                'element': '0',
-                'active_material': 'patent',
-                'active_color': 'mat_patent_red'
-            },
-        ]
     },
     'product_tour_configuration': {
         'elsevr_women_1_1': {

11e79649aeb08c417ec90142b8f07f9b6e61a0b6
Mertcan <m.yucel@else-corp.it>
2017-11-03 19:30:09 +0100

    fitting page styling for altroconsumo

--- src/config.js
+++ src/config.js
@@ -25,7 +25,7 @@ window.config = {
     'default_material_camera': 'cameraLEFTp',
     'default_part_camera': 'cameraLEFTp',
     'default_preview_camera': 'cameraLEFTp',
-    'default_fitting_camera': 'cameraLEFTp',
+    'default_fitting_camera': 'cameraRIGHTp',
     'camera_animation_time': 1000,
     'default_language': 'en',
     'default_scanner': 'Dome000018',

ad7f3b002ff77c01aed13b36c5a197d8316c6e2b
Mertcan <m.yucel@else-corp.it>
2018-02-09 10:24:13 +0100

    render images added to repo

--- src/config.js
+++ src/config.js
@@ -57,7 +57,7 @@ window.config = {
     'component_cameras': {},
     'cameras_initial_settings': {},
     'home_state': 'PRODUCT_LIST',
-    'render_quality': 'AUTO',
+    'render_quality': 'LOW',
     'staff_login': true
 
 };
